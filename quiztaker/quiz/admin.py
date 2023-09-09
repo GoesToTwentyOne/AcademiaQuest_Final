72172a29c75bd853785beb60782ce09ae5f3a45e
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-09-09 20:08:55 +0600

    done

--- quiztaker/quiz/admin.py
+++ quiztaker/quiz/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from quiz.models import Course,Question,Result,QuizHistory
 
 # Register your models here.
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(Result)
 admin.site.register(QuizHistory)
 # admin.site.register(Course)
 
-
+class QuestionAdmin(admin.ModelAdmin):
+    list_display=['question','course',]
+admin.site.register(Question,QuestionAdmin)
 class QuizModelAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['course_name',]}
 admin.site.register(Course, QuizModelAdmin)
